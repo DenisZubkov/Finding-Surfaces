7e50c472abdc52f2cfaaa98b8f38e043006361c5
Denis Zubkov <denzu@macbook-pro-denis.local>
2018-09-19 18:41:01 +0300

    Final commit

--- Finding Surfaces/ViewController.swift
+++ Finding Surfaces/ViewController.swift
@@ -24,10 +24,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,6 +38,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
+        configuration.planeDetection = [.horizontal]
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -47,7 +51,59 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
+    func createFloor(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        
+        
+        let geometry = SCNPlane(width: width, height: height)
+        
+        let node = SCNNode()
+        node.geometry = geometry
+        
+        node.eulerAngles.x = -Float.pi / 2
+        node.opacity = 0.25
+        node.name = "Floor"
+        return node
+    }
+    
+    func createBox() -> SCNNode {
+        let codeBox = SCNNode()
+        codeBox.position = SCNVector3(0, 0, 0)
+        let box = SCNBox(width: 0.3, height: 0.3, length: 1, chamferRadius: 0)
+        box.firstMaterial?.diffuse.contents = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+        codeBox.geometry = box
+        return codeBox
+    }
     // MARK: - ARSCNViewDelegate
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        guard let plainAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        let floor = createFloor(planeAnchor: plainAnchor)
+        node.addChildNode(floor)
+        
+        
+    }
+    
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let plainAnchor = anchor as? ARPlaneAnchor,
+        let floor = node.childNodes.first,
+        let geometry = floor.geometry as? SCNPlane
+            else { return }
+        let boxNode = createBox()
+        floor.addChildNode(boxNode)
+        geometry.width = CGFloat(plainAnchor.extent.x)
+        geometry.height = CGFloat(plainAnchor.extent.z)
+        
+        floor.position = SCNVector3(plainAnchor.center.x, 0, plainAnchor.center.z)
+        
+        
+    }
     
 /*
     // Override to create and configure nodes for anchors added to the view's session.
